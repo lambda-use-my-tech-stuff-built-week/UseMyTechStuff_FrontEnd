545c52f314f116cef946367ab6d4a10d1d655d76
stevekutz <stkutz@gmail.com>
2019-05-09 22:13:31 -0500

    built login and connected to API

--- usemytechstuff_frontend/src/components/App.js
+++ usemytechstuff_frontend/src/components/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 //import logo from '../logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
 import ItemList from './ItemList';
 
 
 function App() {
   return (
-    <div className="App">
-      <ItemList
-        msgTitle="CheckBox2Status as prop"
-        loaderType="Rings"
-        size="180"
-        color="orange"
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+      </div>
+      <Route
+        path="/login"
+        // component={Login}
+        render={props => <Login {...props} isLoggingIn={false} />}
       />
-    </div>
+      <PrivateRoute exact path="/protected" component={ItemList} />
+    </Router>
   );
 }
 
