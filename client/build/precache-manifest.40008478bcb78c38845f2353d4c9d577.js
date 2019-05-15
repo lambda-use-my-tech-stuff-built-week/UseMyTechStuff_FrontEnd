049217f15be2d4d5284682a35ae089f8583aaf49
stevekutz <stkutz@gmail.com>
2019-05-15 00:07:54 -0500

    latest fix2

---
+++ client/build/precache-manifest.40008478bcb78c38845f2353d4c9d577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a9cd5c4a4b0ed22664c8712dd43d4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d8a036129c739f243e",
+    "url": "/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "d7a1c46c77b988e38d8b",
+    "url": "/static/css/main.ddce542f.chunk.css"
+  },
+  {
+    "revision": "17d8a036129c739f243e",
+    "url": "/static/js/2.d310b8af.chunk.js"
+  },
+  {
+    "revision": "d7a1c46c77b988e38d8b",
+    "url": "/static/js/main.8accae91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
