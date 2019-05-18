b0061e4ddce92c5d113a9f221e949bd54cd4b80a
stevekutz <stkutz@gmail.com>
2019-05-18 14:34:55 -0500

    preparing for Update modal, add fix

--- client/src/components/ModalUpdateTech.js
+++ client/src/components/ModalUpdateTech.js
@@ -1,21 +1,68 @@
 import React from 'react';
+import './addTechForm.css';
+
+import {getTech, deleteTech, updateTech, addTech} from '../actions';
+import { connect } from 'react-redux';
+import { withRouter } from "react-router-dom";
+
+
+
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class ModalUpdateTech extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
 
-    this.toggle = this.toggle.bind(this);
-  }
+  state = {
+    modal: false,
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
+
+    name: '',
+    user_id: localStorage.getItem("user_id"),
+    //   user_id: 23,
+    category: '',
+    description: '',
+    picture: "https://zdnet2.cbsistatic.com/hub/i/r/2018/01/26/b4fe5bfc-6e3b-4575-b8db-f06caadc1a71/thumbnail/770x578/5f810055799b727df363a6e0cfcece38/tech-transport-future-intro.jpg",
+    cost: '',
+    //  availability: true,
+    availability: true,
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({[e.target.name]: e.target.value})
+    //   this.setState({newTodo: e.target.value})
+  };
+
+  handleSubmit = e => {
+    const {name, user_id, category, picture, description, cost, availability} = this.state;
+    e.preventDefault();
+
+    // must fill in ALL fields !!!
+    if(name && category && description && category && cost ) {
+      this.props.addTech({name, user_id, category, picture, description, cost, availability});
+      // this.props.getTech();
+      this.setState({
+        name: '',
+        category: '',
+        description: '',
+        picture: '',
+        cost: '',
+        availability: false,
+      });
+    }
+
+  };
+
+  handleUpdate  = (e, id, tech) => {
+
+    //   const {name, user_id, category, picture, description, cost, availability} = this.state;
+
+
+    e.preventDefault();
+    this.props.updateTech(id, tech);
+  };
+
+
+
 
   render() {
     return (
@@ -24,6 +71,75 @@ class ModalUpdateTech extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Login to UseMyTechstuff !</ModalHeader>
           <ModalBody>
+
+
+
+            <div className = "addTechItem-container">
+              <form
+                className = "addTechItemForm"
+                onSubmit = {this.handleSubmit}
+              >
+
+                <div className = "buttonContainer">
+                  <button
+                    className = "addTechItemButton"
+                  >UPDATE Tech !!!
+                  </button>
+                </div>
+
+                <div className = "boxy">
+
+                  <div className = "techFormInputs">
+                    <input
+                      className= "addInputFieldName"
+                      value = {this.state.name}
+                      name = "name"
+                      type = "text"
+                      placeholder = "name"
+                      onChange={this.handleChange}
+                    />
+                    <input
+                      className= "addInputField"
+                      value = {this.state.category}
+                      name = "category"
+                      type = "text"
+                      placeholder = "category"
+                      onChange={this.handleChange}
+                    />
+
+                    <input
+                      className= "addInputField"
+                      value = {this.state.cost}
+                      name = "cost"
+                      type = "text"
+                      placeholder = "cost"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+
+                  <div className= "addTextAreaField" >
+              <textarea
+                value = {this.state.picture}
+                name = "picture"
+                placeholder = "picture"
+                onChange={this.handleChange}
+              />
+                  </div>
+
+                  <div className = "addTextAreaField">
+               <textarea
+                 value = {this.state.description}
+                 name = "description"
+                 wrap="hard"
+                 placeholder = "description"
+                 onChange={this.handleChange}
+               />
+                  </div>
+                </div>
+
+              </form>
+
+            </div>
 
 
 
@@ -39,4 +155,21 @@ class ModalUpdateTech extends React.Component {
   }
 }
 
-export default ModalUpdateTech;
+const mapStateToProps = ({techItems, fetchingData, deletingTech, updatingTech, addingTech}) => ({
+  techItems,
+  fetchingData,
+  deletingTech,
+  updatingTech,
+  addingTech,
+
+
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {getTech, deleteTech, updateTech, addTech}
+
+  )(ModalUpdateTech)
+
+);
