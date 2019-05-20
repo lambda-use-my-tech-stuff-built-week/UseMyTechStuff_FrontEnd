2e3a5e4ad42d984bdbac1eea6d6ef52e37983e52
stevekutz <stkutz@gmail.com>
2019-05-19 22:01:31 -0500

    applied modals, cards, adding exported styled components

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -4,6 +4,30 @@ import { getUsers, } from '../actions/userData';
 import axios from 'axios';
 import styled from 'styled-components';
 
+
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+
+const UserCard2 = (users) => {
+  return (
+    <MDBCol>
+      <MDBCard style={{ width: "22rem" }}>
+        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            <h3>Username: {users.username}</h3>
+            <p>Email: {users.email}</p>
+          </MDBCardText>
+          <MDBBtn href="#">MDBBtn</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+  )
+}
+
+
+// import UserCard2 from './MDBCard';
+
 const UserWrapper = styled.div`
   text-align: center;
   display: flex;
@@ -30,6 +54,17 @@ const UserCard = styled.div`
 `;
 
 
+ const UserName = styled.div`
+  font-family: 'Merienda', cursive;
+  font-size: 2rem;
+ `;
+
+ const EmailName = styled.div`
+  font-family: Dosis;
+  font-size: 1.25rem;
+ 
+ `;
+
 class UserList extends Component {
 
   componentDidMount() {
@@ -52,34 +87,61 @@ class UserList extends Component {
       })
   }
 
+  getRandomPic = () => {
+    return  'https://picsum.photos/100/100';
+  };
+
   state = {
     users: [],
-  }
+  };
   render() {
     return (
       <div>
         <Title>Techies</Title>
         <UserWrapper>
           {this.state.users.map((users, id) => (
-            <UserCard>
-              <h3>Username: {users.username}</h3>
-              <p>Email: {users.email}</p>
-            </UserCard>
+            <div key = {id}>
+
+              <MDBCol>
+                <MDBCard style={{ width: "22rem" }}>
+                  <MDBCardImage className="img-fluid" src={this.getRandomPic()} waves />
+                  <MDBCardBody>
+                    <MDBCardText>
+                      <UserName>{users.username}</UserName>
+                      <EmailName>{users.email}</EmailName>
+                    </MDBCardText>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+
+
+            </div>
+
+
+
           ))}
         </UserWrapper>
       </div>
     )
   }
-};
+}
 
 
 const mapStateToProps = state => {
   return {
     users: state.users
   }
-}
+};
 
 export default connect(
   mapStateToProps,
   { getUsers, }
 )(UserList);
+
+
+/*
+<UserCard>
+              <h3>Username: {users.username}</h3>
+              <p>Email: {users.email}</p>
+            </UserCard>
+ */
---
+++ client/src/styled/LoginStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+
+const LoginContainer = styled.div`
+  border: 1px solid green;
+  width: 50%;
+  padding: 4px;
+  margin: 3px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+
+
+
+
+export {LoginContainer};
